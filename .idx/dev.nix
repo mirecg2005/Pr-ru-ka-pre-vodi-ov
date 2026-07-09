4f31c13551896e9fc7cd0274fea98cde16b7e302
mirecg2005 <mirecg2005@gmail.com>
2026-07-09 19:23:30 +0200

    Pridanie konfiguracie pre Project IDX

---
+++ .idx/dev.nix
@@ -0,0 +1,32 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Channel to use.
+  channel = "stable-23.11";
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20
+  ];
+
+  # Sets environment variables in the workspace
+  env = {};
+
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "google.idx-ai"
+    ];
+
+    # Enable previews and configure hot-reloading
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["npx" "-y" "http-server" "-p" "$PORT" "-c-1"];
+          manager = "web";
+        };
+      };
+    };
+  };
+}
